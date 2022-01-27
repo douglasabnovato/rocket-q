6046eec7e186ec53d0da94346f9ab86c566bb605
Douglas A B Novato <douglasabnovato@gmail.com>
2022-01-27 12:26:37 -0300

    revisando alguns detalhes do código

--- src/server.js
+++ src/server.js
@@ -4,14 +4,10 @@ const path = require('path')
 
 const server = express()
 
-server.set('view engine', 'ejs')
-
-server.use(express.static("public"))
-
-server.set('views', path.join(__dirname, 'views'))
-
-server.use(express.urlencoded({extended: true}))
-
+server.set('view engine', 'ejs') 
+server.use(express.static("public")) 
+server.set('views', path.join(__dirname, 'views')) 
+server.use(express.urlencoded({extended: true})) 
 server.use(route)
 
 server.listen(3000, () => console.log("RODANDO"))
