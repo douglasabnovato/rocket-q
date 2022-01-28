f8da7abcf740b0e6fe1af51bf2f72cabc78449de
Douglas A B Novato <douglasabnovato@gmail.com>
2022-01-28 17:36:57 -0300

    configuração do sqlite

--- src/db/init.js
+++ src/db/init.js
@@ -7,15 +7,13 @@ const initDb = {
         await db.exec(`CREATE TABLE rooms (
             id INTEGER PRIMARY KEY,
             pass TEXT
-        )`);
-
+        )`); 
         await db.exec(`CREATE TABLE questions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT,
             read INT,
             room INT
-        )`);
-
+        )`); 
         await db.close()
     }
 }
